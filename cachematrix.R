f94cc0254b1587f0c302e8cb8a577a42ef8e5522
AndrewBergemann <andrew.d.bergemann@hofstra.edu>
2014-07-24 16:11:19 -0500

    I have introduced two new functions
    These functions are to check for stored inversions of the matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions combined save computational time by preventing the repeated calculation of inverse matrices. Before calculating a fresh inversion, the functions check whether previous inversions of the matrix have been stored, and if so retrieve them from storage.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function, makeCacheMatrix, caches matrices and there inverse partners.
 
+makeCacheMatrix <- function(a) 
+{
+b<-matrix(a,sqrt(length(a)),sqrt(length(a)))
+	    m<-NULL
+        set <- function(y) {
+                b <<- y
+                m<<-NULL
+        }
+        b<-matrix(a,sqrt(length(a)),sqrt(length(a)))
+        get <- function() b
+        setmatrix <- function (solve) m <<- setmatrix
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
+             
 }
 
 
-## Write a short comment describing this function
+## This function checks for previous inversions of a matrix. If it does not find any it creates a new inversion and stores it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a, ...) 
+{
+        m <- a$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- a$get()
+        m <- solve (data, ...)
+        a$setmatrix(m)
+        m
 }
